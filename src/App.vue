bbbcaa3121c374f742589a3e3a16523fbf9ccfb5
ahmadZaid <ahmaadzaid7@gmail.com>
2022-10-21 01:59:33 +0300

    add form vaildation

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,43 @@
 <template>
   <!-- https://github.com/paladinescamila/Interactive-Card-Details-Form -->
+  <!-- https://github.com/cosmoart/Interactive-card-details-form -->
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <div
     class="
       flex flex-col
       lg:flex-row
-      justify-between
+      justify-center
+      lg:justify-evenly
       items-center
+      min-h-screen
       font-SpaceGrotesk
-      max-w-7xl
-      container
+      relative
     "
   >
+    <div
+      id="gradient1"
+      class="w-full h-[240px] lg:max-w-lg lg:h-full absolute top-0 left-0"
+    ></div>
     <!-- graident div -->
-    <div id="gradient" class="w-full lg:w-[483px] h-[10%] lg:min-h-screen">
+    <div id="gradient">
       <div
         class="
           flex flex-col
           justify-center
           items-center
-          space-y-10
-          mt-4
-          lg:mt-20 lg:translate-x-20
+          lg:space-y-2
+          -translate-y-5
         "
       >
         <!-- card front   -->
         <div
           class="
             relative
-            shrink
             overflow-hidden
             order-2
             lg:order-1
-            translate-y-20
-            lg:translate-y-0
             z-10
+            -translate-y-14
           "
         >
           <img
@@ -60,7 +63,7 @@
                 lg:text-2xl
                 font-bold
                 tracking-widest
-                ml-3
+                ml-1
                 lg:ml-8
               "
             >
@@ -73,7 +76,7 @@
                 lg:text-2xl
                 font-bold
                 tracking-widest
-                ml-3
+                ml-2
                 lg:ml-8
               "
             >
@@ -81,7 +84,7 @@
             </h1>
           </div>
 
-          <div class="absolute bottom-1 left-2 text-white">
+          <div class="absolute bottom-4 left-2 text-white">
             <p
               class="
                 uppercase
@@ -101,7 +104,7 @@
               AHMAD ALHARBI
             </p>
           </div>
-          <div class="absolute bottom-1 right-2 text-white">
+          <div class="absolute bottom-4 right-2 text-white">
             <p class="text-[12px]" v-if="mm || yy">{{ mm }}/{{ yy }}</p>
             <p class="text-[12px]" v-else>00/00</p>
           </div>
@@ -115,17 +118,7 @@
         </div>
 
         <!-- card back   -->
-        <div
-          class="
-            lg:translate-x-20
-            order-1
-            lg:order-2
-            translate-y-36
-            lg:translate-y-0
-            translate-x-10
-            z-0
-          "
-        >
+        <div class="lg:translate-x-20 order-1 lg:order-2 translate-x-10 z-0">
           <div class="relative">
             <img
               class="w-[286px] lg:w-[447px] lg:h-[245px]"
@@ -167,8 +160,16 @@
       </div>
     </div>
     <!-- form div -->
-    <div class="flex-initial shrink-0 pr-5 mt-28 lg:mt-0">
-      <form class="flex flex-col justify-center items-center">
+    <div v-if="!done" class="px-10 py-6">
+      <form
+        @submit.prevent="handleSubmit"
+        class="
+          flex flex-col
+          justify-center
+          items-center
+          animate__animated animate__pulse
+        "
+      >
         <div class="text-left my-6">
           <label
             class="
@@ -191,9 +192,11 @@
               rounded-2xl
               placeholder-LightGrey
             "
+            :class="{ 'border-red , text-red': errorName }"
             v-model="name"
             placeholder="e.g.Ahmad Alharbi"
           />
+          <p :class="{ 'text-red': errorName }">{{ errorName }}</p>
         </div>
         <div class="text-left">
           <label
@@ -207,8 +210,10 @@
             >card number</label
           >
           <input
+            maxlength="16"
             v-model="number"
             type="text"
+            :class="{ 'border-red , text-red': errorNumber }"
             class="
               block
               w-[381px]
@@ -219,6 +224,7 @@
             "
             placeholder="e.g. 1234 5678 9123 0000"
           />
+          <p :class="{ 'text-red': errorNumber }">{{ errorNumber }}</p>
         </div>
         <div class="flex mt-10">
           <div>
@@ -235,12 +241,16 @@
               >Exp.Date <span class="ml-1"> (MM/YY)</span></label
             >
             <input
+              maxlength="2"
               v-model="mm"
               type="text"
               class="w-[80px] border-gray-300 rounded-2xl placeholder-LightGrey"
               placeholder="MM"
+              :class="{ 'border-red , text-red': errorMM }"
             />
+
             <input
+              maxlength="2"
               v-model="yy"
               type="text"
               class="
@@ -250,6 +260,7 @@
                 rounded-2xl
                 placeholder-LightGrey
               "
+              :class="{ 'border-red , text-red': errorYY }"
               placeholder="YY"
             />
           </div>
@@ -267,6 +278,7 @@
               >CVC</label
             >
             <input
+              maxlength="3"
               v-model="cvc"
               type="text"
               class="
@@ -275,10 +287,20 @@
                 rounded-2xl
                 placeholder-LightGrey
               "
+              :class="{ 'border-red , text-red': errorCVC }"
               placeholder="e.g.123"
             />
           </div>
         </div>
+        <div class="flex justify-between items-center space-x-6">
+          <p v-if="errorMM || errorYY" class="-translate-x-32 text-red">
+            Can’t be blank
+          </p>
+          <p class="translate-x-24" :class="{ 'text-red': errorCVC }">
+            {{ errorCVC }}
+          </p>
+        </div>
+
         <button
           class="
             bg-DeepViolet
@@ -293,7 +315,40 @@
         >
           Confirm
         </button>
+        <p class="text-red text-md" v-if="checkAllField">
+          Please full in all fields
+        </p>
       </form>
+    </div>
+    <!-- thank you message -->
+    <div
+      v-if="done"
+      class="
+        animate__animated animate__fadeInDownBig
+        flex flex-col
+        justify-center
+        items-center
+        transition-all
+      "
+    >
+      <img src="./assets/images/icon-complete.svg" alt="" />
+      <h1 class="text-4xl text-DeepViolet my-4">Thank you</h1>
+      <p class="text-PurplishGrey">We’ve added your card details</p>
+      <button
+        @click="backHome"
+        class="
+          bg-DeepViolet
+          text-white
+          font-bold
+          text-2xl
+          w-full
+          py-3
+          mt-6
+          rounded-md
+        "
+      >
+        Continue
+      </button>
     </div>
   </div>
 </template>
@@ -308,7 +363,74 @@ export default {
       mm: "",
       yy: "",
       cvc: "",
+      errorName: "",
+      errorNumber: "",
+      errorMM: "",
+      errorYY: "",
+      errorCVC: "",
+      done: false,
+      errorCount: 0,
+      checkAllField: false,
     };
+  },
+  methods: {
+    handleSubmit() {
+      // this.errorName = "";
+      //
+      this.errorNumber = "";
+      this.errorMM = "";
+      this.errorYY = "";
+      this.errorCVC = "";
+      this.checkAllField = false;
+      this.errorCount = 0;
+      let letters = /^[A-Za-z]+$/;
+      let numbers = /^\d+$/;
+      if (
+        this.name == "" ||
+        this.number == "" ||
+        this.mm == "" ||
+        this.yy == "" ||
+        this.cvc == ""
+      ) {
+        this.errorCount++;
+        this.checkAllField = true;
+      }
+      // if (!this.name.match(letters)) {
+      //   this.errorName = "Wrong format, letters only";
+      // }
+      if (!this.number.match(numbers) && this.number.length <= 16) {
+        this.errorNumber = "Wrong format, numbers only and 16 digits";
+        errorCount++;
+      }
+      if (!this.mm.match(numbers)) {
+        this.errorMM = "Can’t be blank";
+        errorCount++;
+      }
+      if (!this.yy.match(numbers)) {
+        this.errorYY = "Can’t be blank";
+        errorCount++;
+      }
+      if (!this.cvc.match(numbers)) {
+        this.errorCVC = "Can’t be blank";
+        errorCount++;
+      }
+
+      if (this.errorCount === 0) {
+        this.done = true;
+        this.number = this.number
+          .replace(/[^\dA-Z]/g, "")
+          .replace(/(.{4})/g, "$1 ")
+          .trim();
+      }
+    },
+    backHome() {
+      this.name = "";
+      this.number = "";
+      this.mm = "";
+      this.yy = "";
+      this.cvc = "";
+      this.done = false;
+    },
   },
 };
 </script>
